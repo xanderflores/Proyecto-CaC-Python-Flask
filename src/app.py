92249ccee23ce91f9363afae84e877900ce9694c
xanderflores <alexanderflores_10_@hotmail.es>
2024-07-13 21:31:00 -0300

    iniciando el  las configuracion para conectarse a la base de datos mysql

--- src/app.py
+++ src/app.py
@@ -5,6 +5,13 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='burn_reborn'
+
+mysql.init_app(app)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
